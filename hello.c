8f706e0288087dd0f480f3fe194ed4c990cefbca
vigneshwaran <vigneshwaran.6363@gmail.com>
2020-08-24 01:35:32 -0700

    inducing warnings ....ect

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
+	//not used and initialized variable
+	int i;
 	printf("Hello Universe!!!\n");
 	return 0;
+	//non-reachable code
+	printf("non-reachable code ..........\n");
 }
